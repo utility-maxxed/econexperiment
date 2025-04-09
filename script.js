99d23fe4597c08dc53fdfd6f868c018caf03ce99
utility-maxxed <sflemming925@gmail.com>
2025-04-08 23:58:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-document.getElementById("experimentForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  const decision = document.getElementById("decision").value;
-  alert("Thanks! You chose: " + decision);
-  // Optional: send to Google Sheets
-});
+
